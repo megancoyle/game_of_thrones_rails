ab976a2e22a1215f4203e57e7e74c6b00ca0d5db
Megan Coyle <meg@mcoyle.com>
2016-08-01 17:23:05 -0400

    Add CRUD for characters

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,9 +1,48 @@
 class CharactersController < ApplicationController
+  def index
+    @characters = Character.all
+  end
+
+  def show
+    @character = Character.find(params[:id])
+  end
+
   def new
+    @character = Character.new
+  end
+
+  def edit
+    @character = Character.find(params[:id])
   end
 
   def create
-    render plain: params[:character].inspect
+    @character = Character.new(character_params)
+
+    if @character.save
+      redirect_to @character
+    else
+      render 'new'
+    end
   end
 
+  def update
+    @character = Character.find(params[:id])
+
+    if @character.update(character_params)
+      redirect_to @character
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to characters_path
+  end
+
+  private
+    def character_params
+      params.require(:character).permit(:name, :img_url, :gender, :house_id)
+    end
 end
