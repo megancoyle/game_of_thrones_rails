3daed886ec309dd481a0b962d15cfd35d6ba2f54
Megan Coyle <meg@mcoyle.com>
2016-08-01 18:20:19 -0400

    Add character images and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 House.destroy_all
+Character.destroy_all
 
 house_list = [
   ["House Arryn", "/images/arryn.jpg"],
@@ -16,4 +17,25 @@ house_list = [
 
 house_list.each do |name, image|
   House.create(name: name, img_url: image)
+end
+
+character_list = [
+  ["Tyrion Lannister", "Male", "/images/characters/tyrion.jpg", 6],
+  ["Jaime Lannister", "Male", "/images/characters/jaime.jpg", 6],
+  ["Cersei Lannister", "Female", "/images/characters/cersei.jpg", 6],
+  ["Daenerys Targaryen", "Female", "/images/characters/daenerys.jpg", 9],
+  ["John Snow", "Male", "/images/characters/johnsnow.jpg", 8],
+  ["Davos Seaworth", "Male", "/images/characters/davos.jpg", 2],
+  ["Melisandre", "Female", "/images/characters/melisandre.jpg", 2],
+  ["Sansa Stark", "Female", "/images/characters/sansa.jpg", 8],
+  ["Arya Stark", "Female", "/images/characters/arya.jpg", 8],
+  ["Bran Stark", "Male", "/images/characters/bran.jpg", 8],
+  ["Ramsay Bolton", "Male", "/images/characters/ramsay.jpg", 3],
+  ["Walder Frey", "Male", "/images/characters/walder.jpg", 4],
+  ["Nymeria Sand", "Fmale", "/images/characters/nymeria.jpg", 7],
+  ["Theon Greyjoy", "Male", "/images/characters/theon.jpg", 5]
+]
+
+character_list.each do |name, gender, image, house|
+  Character.create(name: name, gender: gender, img_url: image, house_id: house)
 end
