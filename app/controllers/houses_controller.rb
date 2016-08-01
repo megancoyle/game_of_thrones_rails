c66d07883bc87423108ed4d01c9b59d73a902d5c
Megan Coyle <meg@mcoyle.com>
2016-08-01 16:44:46 -0400

    Add editing and creating new houses functionality

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -8,16 +8,35 @@ class HousesController < ApplicationController
   end
 
   def new
+    @house = House.new
+  end
+
+  def edit
+    @house = House.find(params[:id])
   end
 
   def create
     @house = House.new(house_params)
-    @house.save
-    redirect_to @house
+
+    if @house.save
+      redirect_to @house
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @house = House.find(params[:id])
+
+    if @house.update(house_params)
+      redirect_to @house
+    else
+      render 'edit'
+    end
   end
 
   private
     def house_params
-      params.require(:house).permit(:name)
+      params.require(:house).permit(:name, :img_url)
     end
 end
--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,5 @@
 class House < ApplicationRecord
+  validates :name, presence: true,
+                      length: { minimum: 6 }
+  validates :img_url, presence: true
 end
